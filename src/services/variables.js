ab91411a3df2ccaadd82f9cb415f108a75120587
Renan Fernandes <renzinestuods@gmail.com>
2022-09-23 17:08:56 -0300

    Cria variaves da chaves do local storage

---
+++ src/services/variables.js
@@ -0,0 +1,7 @@
+export const MEALSTOKEN = 'mealsToken';
+export const DRINKS_TOKEN = 'drinksToken';
+export const DONE_RECIPES = 'doneRecipes';
+export const FAVORITE_RECIPES = 'favoriteRecipes';
+export const IN_PROGRESS_RECIPES = 'inProgressRecipes';
+const USER = 'user';
+export default USER;
