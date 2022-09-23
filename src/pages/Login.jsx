86327304cdc79471d1a6d07f3a4100e51d019ad8
Renan Fernandes <renzinestuods@gmail.com>
2022-09-23 16:15:24 -0300

    Tela de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const MIN_PASSWORD_CHARACTERS = 6;
+const REGEXP_EMAIL = /\S+@\S+\.\S+/;
+
+export default function Login() {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = login;
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLogin((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const isValid = (password.length > MIN_PASSWORD_CHARACTERS) && REGEXP_EMAIL.test(email);
+
+  return (
+    <form>
+      <label htmlFor="email-input">
+        Email
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          id="email-input"
+          value={ email }
+          onChange={ handleChange }
+        />
+      </label>
+      <label htmlFor="password-input">
+        Senha
+        <input
+          type="text"
+          data-testid="password-input"
+          name="password"
+          id="password-input"
+          value={ password }
+          onChange={ handleChange }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ !isValid }
+      >
+        Enter
+      </button>
+    </form>
+  );
+}
