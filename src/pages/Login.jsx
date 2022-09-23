124660bff6dc866cea9e7da39699917e1b797031
Renan Fernandes <renzinestuods@gmail.com>
2022-09-23 17:25:27 -0300

    Salva no localstorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
+import saveItem from '../helpers/storage';
+import USER, { DRINKS_TOKEN, MEALSTOKEN } from '../services/variables';
 
 const MIN_PASSWORD_CHARACTERS = 6;
 const REGEXP_EMAIL = /\S+@\S+\.\S+/;
 
 export default function Login() {
-  const [login, setLogin] = useState({
+  const [{ email, password }, setLogin] = useState({
     email: '',
     password: '',
   });
-  const { email, password } = login;
 
   const handleChange = ({ target: { name, value } }) => {
     setLogin((prevState) => ({
@@ -18,6 +19,12 @@ export default function Login() {
   };
 
   const isValid = (password.length > MIN_PASSWORD_CHARACTERS) && REGEXP_EMAIL.test(email);
+
+  const saveUser = () => {
+    saveItem(USER, { email });
+    saveItem(MEALSTOKEN, 1);
+    saveItem(DRINKS_TOKEN, 1);
+  };
 
   return (
     <form>
@@ -47,6 +54,7 @@ export default function Login() {
         type="button"
         data-testid="login-submit-btn"
         disabled={ !isValid }
+        onClick={ saveUser }
       >
         Enter
       </button>
