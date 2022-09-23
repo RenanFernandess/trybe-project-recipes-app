a77502b5ece0e60048be0b76c494b5bb2b3eda6f
gustavoBarros <gustavo.dutra11@gmail.com>
2022-09-23 18:13:29 -0300

    Fix main-group-31

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,10 +10,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
-// .createRoot()
-// .render();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
