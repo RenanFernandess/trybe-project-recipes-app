67efec84742c90b28a148b97850d4595948c8c5a
gustavoBarros <gustavo.dutra11@gmail.com>
2022-09-23 15:08:54 -0300

    [GRUPO 31] - Iniciando Projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('Farewell, front-end', () => {
+test('I will test your application', () => {
   // Este arquivo pode ser modificado ou deletado sem problemas
   render(<App />);
   const linkElement = screen.getByText(/TRYBE/i);
