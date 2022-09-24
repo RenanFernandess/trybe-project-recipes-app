578ea586599f22893689e619d7b7a1b0d66b2da8
Renan Fernandes <renzinestuods@gmail.com>
2022-09-24 13:13:09 -0300

    REQUISITO-8

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import imageProfile from '../images/profileIcon.svg';
 import imageSearch from '../images/searchIcon.svg';
 
 const PAGES_TITLE = ['Meals', 'Drinks'];
 export default function Header({ title }) {
+  const history = useHistory();
   return (
-
     <div>
       <h2
         className="profile-title"
@@ -14,12 +15,17 @@ export default function Header({ title }) {
       >
         {title}
       </h2>
-      <img
-        className="profile-image"
-        data-testid="profile-top-btn"
-        src={ imageProfile }
-        alt="profile"
-      />
+      <button
+        type="button"
+        onClick={ () => { history.push('/profile'); } }
+      >
+        <img
+          className="profile-image"
+          data-testid="profile-top-btn"
+          src={ imageProfile }
+          alt="profile"
+        />
+      </button>
       {PAGES_TITLE.some((item) => item === title) && (
         <img
           className="profile-search"
