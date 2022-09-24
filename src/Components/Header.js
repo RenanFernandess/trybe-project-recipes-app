acdc262d16c42567c963e7022d8dc01dd69d18a2
gustavoBarros <gustavo.dutra11@gmail.com>
2022-09-24 14:47:40 -0300

    Adiciona placeholder à barra de busca

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -59,6 +59,7 @@ export default function Header({ title }) {
         && <input
           type="text"
           data-testid="search-input"
+          placeholder="Digite aqui sua busca"
         /> }
       </div>
     </div>
