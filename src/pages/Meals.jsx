e4e663215a3fd239091f4f9dee2b3dfce71c6729
Paulo Henrique <paulo.henrique.hunter@gmail.com>
2022-09-26 16:11:04 -0300

    Finaliza os requisitos 16, 17 e 18

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Components/Header';
 // import appContext from '../context/appContext';
+import Footer from '../Components/Footer';
 
 export default function Meals() {
   return (
     <div>
       <Header title="Meals" />
+      <Footer/>
     </div>
   );
 }
