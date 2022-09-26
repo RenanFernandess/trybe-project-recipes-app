cd667cec55e0cad1f05a8410483ef0eba6d764ab
Renan Fernandes <renzinestuods@gmail.com>
2022-09-26 16:20:43 -0300

    Cria os cards das receitas

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-export default function RecipeCard() {
+export default function RecipeCard({ name, thumb, index }) {
   return (
-    <div>RecipeCard</div>
+    <div data-testid={ `${index}-recipe-card` }>
+      <img
+        src={ thumb }
+        alt={ name }
+        data-testid={ `${index}-card-img` }
+      />
+      <p
+        data-testid={ `${index}-card-name` }
+      >
+        { name }
+      </p>
+    </div>
   );
 }
+
+RecipeCard.propTypes = {
+  name: propTypes.string,
+  thumb: propTypes.string,
+  index: propTypes.number,
+}.isRequired;
