df44dd9c0f9761c36d9066f9e401e49b9a4a9cab
Renan Fernandes <renzinestuods@gmail.com>
2022-09-26 15:45:37 -0300

    Faz a requisição para a API e renderiza o componente Recipes

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
 // import appContext from '../context/appContext';
+import Recipes from '../Components/Recipes';
+import fetchAPI from '../hooks/useFetchAPI';
+import { MEALS_ENDPOINT, FIRST_TWELVE } from '../services/variables';
 
 export default function Meals() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    fetchAPI(MEALS_ENDPOINT, ({ meals: result }) => {
+      setMeals(result.slice(0, FIRST_TWELVE));
+    });
+  }, []);
+
   return (
     <div>
       <Header title="Meals" />
+      <Recipes meals={ meals } />
     </div>
   );
 }
