d364ab21f7e2d960db41a0adc63ecab1f4459a48
Renan Fernandes <renzinestuods@gmail.com>
2022-09-26 16:54:47 -0300

    Faz a requisição para a API com o endpoint de drinks e renderiza o componente Recipes

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
+import Recipes from '../Components/Recipes';
+import { DRINKS_ENDPOINT, FIRST_TWELVE } from '../services/variables';
+import fetchAPI from '../hooks/useFetchAPI';
 
 export default function Drinks() {
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    fetchAPI(DRINKS_ENDPOINT, ({ drinks: result }) => {
+      setDrinks(result.slice(0, FIRST_TWELVE));
+    });
+  }, []);
+
   return (
     <div>
       <Header title="Drinks" />
+      <Recipes drinks={ drinks } />
     </div>
-
   );
 }
