452a844a02c5d2a47480001a2e0c5ff6c8248a3c
gustavoBarros <gustavo.dutra11@gmail.com>
2022-09-27 15:23:20 -0300

    Refatoração requisito 11 e 12

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import propTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import imageProfile from '../images/profileIcon.svg';
 import imageSearch from '../images/searchIcon.svg';
-import fetchAPI from '../helpers/fetchAPI';
-
 import '../css/Header.css';
+import appContext from '../context/appContext';
 
 const PAGES_TITLE = ['Meals', 'Drinks'];
+
+const NAME_LENGTH_ERROR = 'Your search must have only 1 (one) character';
+
+export const URLS_BY_PAGE = {
+  Meals: {
+    nome(name) {
+      return `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    },
+    ingredient(name) {
+      return `https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`;
+    },
+    firstLetter(name) {
+      if (name.length > 1) {
+        return global.alert(NAME_LENGTH_ERROR);
+      }
+      return `https://www.themealdb.com/api/json/v1/1/search.php?f=${name}`;
+    },
+  },
+  Drinks: {
+    nome(name) {
+      return `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+    },
+    ingredient(name) {
+      return `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`;
+    },
+    firstLetter(name) {
+      if (name.length > 1) {
+        return global.alert(NAME_LENGTH_ERROR);
+      }
+      return `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${name}`;
+    },
+  },
+};
+
 export default function Header({ title }) {
   const history = useHistory();
+
   const [searchBarBoolean, setSearchBarBoolean] = useState(false);
   const [radioSearch, setRadioSearch] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
+  const {
+    setURL,
+  } = useContext(appContext);
   const handleSearchBoolean = () => {
     if (searchBarBoolean === false) {
       setSearchBarBoolean(true);
@@ -30,29 +67,12 @@ export default function Header({ title }) {
     setSearchTerm((prevState) => ({ ...prevState, name: value }));
   };
 
-  const fetchSearchFilter = () => {
+  const setSearchCategory = () => {
     const { category } = radioSearch;
     const { name } = searchTerm;
-    switch (category) {
-    case 'nome':
-      return fetchAPI(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`, (data) => {
-        console.log(data);
-      });
-    case 'ingredient':
-      return fetchAPI(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`, (data) => {
-        console.log(data);
-      });
-    case 'first-letter':
-      if (name.length > 1) {
-        return global.alert('Your search must have only 1 (one) character');
-      }
-      return fetchAPI(`https://www.themealdb.com/api/json/v1/1/search.php?f=${name}`, (data) => {
-        console.log(data);
-      });
-
-    default:
-      return null;
-    }
+    const getEndPoint = URLS_BY_PAGE[title][category];
+    console.log(getEndPoint);
+    setURL(getEndPoint(name));
   };
 
   console.log(radioSearch);
@@ -130,7 +150,7 @@ export default function Header({ title }) {
             id="first-letter"
             name="category"
             onChange={ handleRadioChange }
-            value="first-letter"
+            value="firstLetter"
           />
           Primeira letra
         </label>
@@ -140,7 +160,7 @@ export default function Header({ title }) {
           id="button"
           name="search-button"
           className="search-button"
-          onClick={ fetchSearchFilter }
+          onClick={ setSearchCategory }
         >
           Busca
         </button>
