ca26cf5be026cef273a11ae0191fbd096954d068
Renan Fernandes <renzinestuods@gmail.com>
2022-09-27 16:25:38 -0300

    Renderiza as categorias

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -2,31 +2,44 @@ import React from 'react';
 import propTypes from 'prop-types';
 import RecipeCard from './RecipeCard';
 
-export default function Recipes({ meals, drinks }) {
-  const recipes = meals || drinks;
-
+export default function Recipes({ recipes, categorys }) {
   return (
     <section>
-      {
-        recipes.map(({
-          strMealThumb,
-          strDrinkThumb,
-          strMeal,
-          strDrink,
-        }, index) => {
-          const thumb = strMealThumb || strDrinkThumb;
-          const name = strMeal || strDrink;
+      <aside>
+        {
+          categorys.map(({ strCategor }) => (
+            <button
+              type="button"
+              key={ strCategor }
+              data-testid={ `${strCategor}-category-filter` }
+            >
+              { strCategor }
+            </button>
+          ))
+        }
+      </aside>
+      <section>
+        {
+          recipes.map(({
+            strMealThumb,
+            strDrinkThumb,
+            strMeal,
+            strDrink,
+          }, index) => {
+            const thumb = strMealThumb || strDrinkThumb;
+            const name = strMeal || strDrink;
 
-          return (
-            <RecipeCard
-              key={ name }
-              name={ name }
-              thumb={ thumb }
-              index={ index }
-            />
-          );
-        })
-      }
+            return (
+              <RecipeCard
+                key={ name }
+                name={ name }
+                thumb={ thumb }
+                index={ index }
+              />
+            );
+          })
+        }
+      </section>
     </section>
   );
 }
