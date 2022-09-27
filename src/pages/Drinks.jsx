20442e5d3b682133cd7b0e2f2d762ac8b50101f4
Renan Fernandes <renzinestuods@gmail.com>
2022-09-27 16:32:32 -0300

    FAz o fetch da categorias

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,29 +1,38 @@
 import React, { useState, useEffect, useContext } from 'react';
 import Header from '../Components/Header';
 import Recipes from '../Components/Recipes';
-import { DRINKS_ENDPOINT, FIRST_TWELVE } from '../services/variables';
+import {
+  DRINKS_CATEGORY_ENDPOINT,
+  DRINKS_ENDPOINT,
+  FIRST_FIVE,
+  FIRST_TWELVE,
+} from '../services/variables';
 import fetchAPI from '../helpers/fetchAPI';
 import Footer from '../Components/Footer';
 import appContext from '../context/appContext';
 
 export default function Drinks() {
-  const {
-    URL,
-  } = useContext(appContext);
+  const { URL } = useContext(appContext);
   const [drinks, setDrinks] = useState([]);
-
+  const [categorys, setCategorys] = useState([]);
   const endPoint = URL || DRINKS_ENDPOINT;
 
   useEffect(() => {
     fetchAPI(endPoint, ({ drinks: result }) => {
       setDrinks(result.slice(0, FIRST_TWELVE));
     });
+    fetchAPI(DRINKS_CATEGORY_ENDPOINT, ({ drinks: result }) => {
+      setCategorys(result.slice(0, FIRST_FIVE));
+    });
   }, [endPoint]);
 
   return (
     <div>
       <Header title="Drinks" />
-      <Recipes drinks={ drinks } />
+      <Recipes
+        recipes={ drinks }
+        categorys={ categorys }
+      />
       <Footer />
     </div>
   );
