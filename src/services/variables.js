4cc589cbc40e8ffc82236c480bb28357210ad979
Renan Fernandes <renzinestuods@gmail.com>
2022-09-27 16:11:06 -0300

    Adicinoa a variavel MEALS_CATEGORY_ENDPOINT e DRINKS_CATEGORY_ENDPOINT

--- src/services/variables.js
+++ src/services/variables.js
@@ -8,5 +8,7 @@ export default USER;
 
 export const MEALS_ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 export const DRINKS_ENDPOINT = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export const MEALS_CATEGORY_ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+export const DRINKS_CATEGORY_ENDPOINT = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 
 export const FIRST_TWELVE = 12;
