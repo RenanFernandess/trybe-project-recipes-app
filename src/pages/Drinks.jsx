938cbb099162b08e85dfd01b35edf6fa648eb380
Renan Fernandes <renzinestuods@gmail.com>
2022-09-27 17:47:58 -0300

    Faz a logica da filtragem por categoria

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -4,6 +4,7 @@ import Recipes from '../Components/Recipes';
 import {
   DRINKS_CATEGORY_ENDPOINT,
   DRINKS_ENDPOINT,
+  DRINKS_FILTER_BY_CATEGOTY_ENDPOINT,
   FIRST_FIVE,
   FIRST_TWELVE,
 } from '../services/variables';
@@ -12,19 +13,27 @@ import Footer from '../Components/Footer';
 import appContext from '../context/appContext';
 
 export default function Drinks() {
-  const { URL } = useContext(appContext);
+  const { URL, setURL } = useContext(appContext);
   const [drinks, setDrinks] = useState([]);
   const [categorys, setCategorys] = useState([]);
-  const endPoint = URL || DRINKS_ENDPOINT;
+  const END_POINT = URL || DRINKS_ENDPOINT;
 
   useEffect(() => {
-    fetchAPI(endPoint, ({ drinks: result }) => {
-      setDrinks(result.slice(0, FIRST_TWELVE));
+    fetchAPI(END_POINT, ({ drinks: recipes }) => {
+      const result = recipes || [];
+      const LAST_INDEX = (result.length < FIRST_TWELVE) ? result.length : FIRST_TWELVE;
+      setDrinks(result.slice(0, LAST_INDEX));
     });
     fetchAPI(DRINKS_CATEGORY_ENDPOINT, ({ drinks: result }) => {
       setCategorys(result.slice(0, FIRST_FIVE));
     });
-  }, [endPoint]);
+  }, [END_POINT, URL]);
+
+  useEffect(() => () => { setURL(''); }, [setURL]);
+
+  const filterByCategory = ({ target: { value } }) => {
+    setURL(`${DRINKS_FILTER_BY_CATEGOTY_ENDPOINT}${value}`);
+  };
 
   return (
     <div>
@@ -32,6 +41,7 @@ export default function Drinks() {
       <Recipes
         recipes={ drinks }
         categorys={ categorys }
+        filterByCategory={ filterByCategory }
       />
       <Footer />
     </div>
