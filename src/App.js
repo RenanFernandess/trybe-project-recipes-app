ff818f016917b07e62cc74b7f36a8b335e566b6b
Renan Fernandes <renzinestuods@gmail.com>
2022-09-28 11:12:39 -0300

    Remove o arquivo App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
