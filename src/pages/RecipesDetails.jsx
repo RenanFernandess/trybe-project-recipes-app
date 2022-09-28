af5ba69c7194f5af4be639c6159f0ed703f5653f
Juliana <julianamq20@gmail.com>
2022-09-28 15:22:28 -0300

    requisito 24

---
+++ src/pages/RecipesDetails.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
+import fetchAPI from '../helpers/fetchAPI';
+import { DRINK_DETAILS, MEALS_DETAILS } from '../services/variables';
+
+export default function RecipesDetails({ match }) {
+  const [recipe, setRecipe] = useState({});
+  const { params: { id }, path } = match;
+  const regex = /^\/meals/i;
+  const urlTest = regex.test(path) ? MEALS_DETAILS : DRINK_DETAILS;
+  console.log(urlTest);
+
+  useEffect(() => {
+    fetchAPI(`${urlTest}${id}`, (data) => {
+      console.log(data);
+      setRecipe(data);
+    });
+  }, [urlTest, id]);
+
+  return (
+    <div>
+      RecipesDetails
+
+    </div>
+  );
+}
+RecipesDetails.propTypes = {
+  history: propTypes.instanceOf(Object),
+}.isRequired;
