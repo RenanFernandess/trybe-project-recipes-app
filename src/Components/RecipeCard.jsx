d7cf7c37652b412707fbd1b351c1654bfa9363ae
Renan Fernandes <renzinestuods@gmail.com>
2022-09-27 21:20:33 -0300

    Adiciona o link para pagina de detalhes da receita

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { Link, useHistory } from 'react-router-dom';
 
-export default function RecipeCard({ name, thumb, index }) {
+export default function RecipeCard({ name, thumb, index, id }) {
+  const { location: { pathname } } = useHistory();
   return (
-    <div data-testid={ `${index}-recipe-card` }>
-      <img
-        width={ 150 }
-        src={ thumb }
-        alt={ name }
-        data-testid={ `${index}-card-img` }
-      />
-      <p
-        data-testid={ `${index}-card-name` }
-      >
-        { name }
-      </p>
-    </div>
+    <Link to={ `${pathname}/${id}` }>
+      <div data-testid={ `${index}-recipe-card` }>
+        <img
+          width={ 150 }
+          src={ thumb }
+          alt={ name }
+          data-testid={ `${index}-card-img` }
+        />
+        <p
+          data-testid={ `${index}-card-name` }
+        >
+          { name }
+        </p>
+      </div>
+    </Link>
   );
 }
 
@@ -23,4 +27,5 @@ RecipeCard.propTypes = {
   name: propTypes.string,
   thumb: propTypes.string,
   index: propTypes.number,
+  id: propTypes.number,
 }.isRequired;
