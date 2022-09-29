369a15956bc179220f2931243a150717c762fdc1
Renan Fernandes <renzinestuods@gmail.com>
2022-09-29 11:15:56 -0300

    Cria um tema para a aplicão

---
+++ src/styles/themes/index.js
@@ -0,0 +1,17 @@
+export default {
+  whith: {
+    colors: {
+      primary: '#FCC436',
+      secundary: '#41197F',
+    },
+
+    textColors: {
+      primary: '#1A1B1C',
+      secundary: '#797D86',
+    },
+
+    borderColor: '#B1B1B1',
+
+    backgroundColor: '#F5F5F5',
+  },
+};
