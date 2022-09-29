113e4e16cd4986d9356a943572b0b44905d5c569
Renan Fernandes <renzinestuods@gmail.com>
2022-09-29 13:26:57 -0300

    Cria o DefaultCard que é o carde principal da receita

---
+++ src/styles/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const DefaultCard = styled.aside`
+  background-color: white;
+  border: 1px solid ${({ theme: { white } }) => white.colors.border};
+  border-radius: 5px;
+  height: 166px;
+  text-decoration: none;
+  width: 163px;
+
+  img {
+    border-radius: 5px 5px 0 0;
+    height: 134px;
+    width: 100%;
+  }
+
+  p {
+    padding: 5px 10px;
+    color: ${({ theme: { white } }) => white.colors.text.primary};
+  }
+`;
+
+export default DefaultCard;
