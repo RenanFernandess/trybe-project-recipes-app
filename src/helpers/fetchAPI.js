4d89d8b74cb76f75ab6ca9cbb9dbfb94e97828d6
Renan Fernandes <renzinestuods@gmail.com>
2022-09-29 20:08:24 -0300

    Cria a função fetchRecipes

--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -1,7 +1,17 @@
+import { FIRST_TWELVE, NO_RECIPES_ERROR } from '../services/variables';
+
 export default function fetchAPI(URL, callback) {
   fetch(URL).then((respose) => respose.json())
     .then(({ meals, drinks }) => {
       const result = meals || drinks || [];
       callback(result);
     });
+}
+
+export function fetchRecipes(endPoint, callback) {
+  fetchAPI(endPoint, (result) => {
+    if (!result.length) return global.alert(NO_RECIPES_ERROR);
+    const LAST_INDEX = (result.length < FIRST_TWELVE) ? result.length : FIRST_TWELVE;
+    callback(result.slice(0, LAST_INDEX));
+  });
 }
