64f7a0f419833df75973008699b02f05b28b8142
Renan Fernandes <renzinestuods@gmail.com>
2022-09-29 10:44:56 -0300

    Faz o map das receitas

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import { getItem } from '../helpers/storage';
+import { DONE_RECIPES } from '../services/variables';
+import DoneRecipeCard from '../Components/DoneRecipeCard';
 
 export default function DoneRecipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const savedRecipes = getItem(DONE_RECIPES) || [];
+    setRecipes(savedRecipes);
+  }, []);
+
   return (
     <div>
       <Header title="Done Recipes" />
-
+      <main>
+        <div>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+          >
+            All
+          </button>
+          <button
+            data-testid="filter-by-meal-btn"
+            type="button"
+          >
+            Meals
+          </button>
+          <button
+            data-testid="filter-by-drink-btn"
+            type="button"
+          >
+            Drinks
+          </button>
+        </div>
+        <section>
+          <p>p</p>
+          {
+            recipes.map(({
+              id,
+              category,
+              name,
+              image,
+              tags,
+              doneDate,
+            }, index) => (
+              <DoneRecipeCard
+                key={ id }
+                name={ name }
+                index={ index }
+                id={ id }
+                image={ image }
+                category={ category }
+                date={ doneDate }
+                tags={ tags }
+              />
+            ))
+          }
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
