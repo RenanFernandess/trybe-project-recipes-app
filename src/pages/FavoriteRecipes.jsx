7b0574980ab274e511954580689c3b50f75a8908
Renan Fernandes <renzinestuods@gmail.com>
2022-09-30 13:33:48 -0300

    Renomeia a função para o nome do component

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Components/Header';
 
-export default function Favorites() {
+export default function FavoriteRecipes() {
   return (
     <div>
       <Header title="Favorite Recipes" />
