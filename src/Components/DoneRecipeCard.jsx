57bbe5ac06e2adab58d30a79636a2697df1ac113
Renan Fernandes <renzinestuods@gmail.com>
2022-09-30 11:17:18 -0300

    Adiciona um botão de compartilhar, renderiza somente 2 categorias, adiciona a nacionalizade

--- src/Components/DoneRecipeCard.jsx
+++ src/Components/DoneRecipeCard.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import shareIcon from '../images/shareIcon.svg';
 
 export default function RecommendationCard({
-  image, name, index, category, date, tags, share,
+  image, name, index, category, date, tags, share, nationality, alcoholicOrNot, id, type,
 }) {
+  const copyBoard = () => {
+    navigator.clipboard.writeText(`http://localhost:3000/${type}s/${id}`);
+    setLinkCopied(true);
+  };
+
   return (
-    <div
-      style={
-        { display: 'flex', flexDirection: 'column' }
-      }
-    >
+    <div>
       <img
         width={ 140 }
         src={ image }
         data-testid={ `${index}-horizontal-image` }
         alt="recommendation-card"
       />
+      <button
+        type="button"
+        onClick={ copyBoard }
+      >
+        <img
+          alt="share"
+          src={ shareIcon }
+        />
+      </button>
       <p
         data-testid={ `${index}-horizontal-name` }
       >
         {name}
       </p>
+      <p>
+        { nationality }
+      </p>
       <p
         data-testid={ `${index}-horizontal-top-text` }
       >
         { category }
+        {' '}
+        { alcoholicOrNot }
       </p>
       <p
         data-testid={ `${index}-horizontal-done-date` }
@@ -39,7 +55,7 @@ export default function RecommendationCard({
       </button>
       <div>
         {
-          tags.map((tag) => (
+          tags.slice(0, 2).map((tag) => (
             <p
               key={ tag }
               data-testid={ `${index}-${tag}-horizontal-tag` }
