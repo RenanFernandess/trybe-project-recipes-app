82561397a9285b7407e7cad5a33e736aebfc0486
Renan Fernandes <renzinestuods@gmail.com>
2022-09-30 12:09:23 -0300

    Ao clicar no nome ou na imagem da receita a pessoa usuaria é redirecionada para tela da receita

--- src/Components/DoneRecipeCard.jsx
+++ src/Components/DoneRecipeCard.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
 
 export default function RecommendationCard({
-  image, name, index, category, date, tags, share, nationality, alcoholicOrNot, id, type,
+  image, name, index, category, date, tags, nationality, alcoholicOrNot, id, type,
 }) {
+  const [linkCopied, setLinkCopied] = useState(false);
+
   const copyBoard = () => {
     navigator.clipboard.writeText(`http://localhost:3000/${type}s/${id}`);
     setLinkCopied(true);
@@ -12,59 +15,59 @@ export default function RecommendationCard({
 
   return (
     <div>
-      <img
-        width={ 140 }
-        src={ image }
-        data-testid={ `${index}-horizontal-image` }
-        alt="recommendation-card"
-      />
-      <button
-        type="button"
-        onClick={ copyBoard }
-      >
+      <Link to={ `/${type}s/${id}` }>
         <img
-          alt="share"
-          src={ shareIcon }
+          width={ 140 }
+          src={ image }
+          data-testid={ `${index}-horizontal-image` }
+          alt="recommendation-card"
         />
-      </button>
-      <p
-        data-testid={ `${index}-horizontal-name` }
-      >
-        {name}
-      </p>
-      <p>
-        { nationality }
-      </p>
-      <p
-        data-testid={ `${index}-horizontal-top-text` }
-      >
-        { category }
-        {' '}
-        { alcoholicOrNot }
-      </p>
-      <p
-        data-testid={ `${index}-horizontal-done-date` }
-      >
-        { date }
-      </p>
-      <button
-        type="button"
-        data-testid={ `${index}-horizontal-share-btn` }
-      >
-        { share }
-      </button>
-      <div>
-        {
-          tags.slice(0, 2).map((tag) => (
-            <p
-              key={ tag }
-              data-testid={ `${index}-${tag}-horizontal-tag` }
-            >
-              { tag }
-            </p>
-          ))
-        }
-      </div>
+        <p
+          data-testid={ `${index}-horizontal-name` }
+        >
+          {name}
+        </p>
+      </Link>
+      <section>
+        <button
+          type="button"
+          onClick={ copyBoard }
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+        >
+          <img
+            alt="share"
+            src={ shareIcon }
+          />
+        </button>
+        {linkCopied && <p>Link copied!</p> }
+        <p
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          { nationality }
+          { ' - ' }
+          { category }
+          {' '}
+          { alcoholicOrNot }
+        </p>
+        <p
+          data-testid={ `${index}-horizontal-done-date` }
+        >
+          { date }
+        </p>
+        <div>
+          {
+            tags.map((tag) => (
+              <p
+                key={ tag }
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+              >
+                { tag }
+              </p>
+            ))
+          }
+        </div>
+      </section>
     </div>
   );
 }
