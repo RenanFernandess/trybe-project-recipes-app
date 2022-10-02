4e0ec6f739da7713d11532d1505f1028211e872e
Juliana <julianamq20@gmail.com>
2022-10-02 17:27:02 -0300

    recipes 100%

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -3,12 +3,20 @@ import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import fetchTotal from '../../cypress/mocks/fetch';
+import mealCategories from '../../cypress/mocks/mealCategories';
+import chickenMeals from '../../cypress/mocks/chickenMeals';
+import meals from '../../cypress/mocks/meals';
 import App from '../App';
 
 describe('Testa o Reciper', () => {
   beforeEach(() => {
-    global.fetch = fetchTotal;
     jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mealCategories)
+        .mockResolvedValueOnce(meals)
+        .mockResolvedValueOnce(fetchTotal)
+        .mockResolvedValueOnce(chickenMeals),
+    });
   });
 
   afterEach(() => jest.clearAllMocks());
@@ -56,5 +64,48 @@ describe('Testa o Reciper', () => {
       const recipes = screen.getAllByTestId(/\S-recipe-card/i);
       expect(recipes).toHaveLength(12);
     });
+  });
+  it('Se todos os botões categorias estão definidos', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mealCategories).mockResolvedValueOnce(meals),
+    });
+    const { history } = renderWithRouter(<App />);
+    history.push('/meals');
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    expect(screen.getByTestId('All-category-filter')).toBeDefined();
+    expect(screen.getByTestId('Chicken-category-filter')).toBeDefined();
+    expect(screen.getByTestId('Beef-category-filter')).toBeDefined();
+    expect(screen.getByTestId('Breakfast-category-filter')).toBeDefined();
+    expect(screen.getByTestId('Dessert-category-filter')).toBeDefined();
+    expect(screen.getByTestId('Goat-category-filter')).toBeDefined();
+  });
+  it('Se há alteração dos botões ', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mealCategories)
+        .mockResolvedValueOnce(chickenMeals),
+    });
+
+    const { history } = renderWithRouter(<App />);
+    history.push('/meals');
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    // quero clicar em chiken e alternar com o beef
+    // tenho que pega qual é o botão de clique
+    // clicar no botão e aparecerá a lista
+    const chikenCategory = await screen.getByTestId('Chicken-category-filter');
+    userEvent.click(chikenCategory, 'Chicken');
+    expect(chikenCategory).toHaveValue('Chicken');
+
+    const beef = await screen.getByTestId('Beef-category-filter');
+    userEvent.click(beef);
+    expect(beef).toHaveValue('Beef');
+
+    const all = await screen.getByTestId('All-category-filter');
+    userEvent.click(beef);
+    expect(all).toHaveValue('All');
   });
 });
