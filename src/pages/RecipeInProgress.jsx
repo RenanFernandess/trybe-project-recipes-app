4b208275f554df6e4f337070ae1ed160b659f15e
Renan Fernandes <renzinestuods@gmail.com>
2022-10-03 19:54:33 -0300

    Desenvolve a tela de receita

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import ShareButton from '../Components/ShareButton';
 
 export default function RecipeInProgress() {
   return (
-    <div>RecipeInProgress</div>
+    <main>
+      <img
+        src=""
+        alt=""
+        data-testid="recipe-photo"
+      />
+      <h1 data-testid="recipe-title">
+        title
+      </h1>
+      <div>
+        <ShareButton
+          testId="share-btn"
+        />
+      </div>
+      <section>
+        <p data-testid="recipe-category">
+          texto da categoria
+        </p>
+        <div data-testid="instructions">
+          instruções
+        </div>
+      </section>
+      <div>
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+        >
+          Finish recipe
+        </button>
+      </div>
+    </main>
   );
 }
