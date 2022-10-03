5a93e9c8aae4610b894f5aad43b54c2539dff42b
Renan Fernandes <renzinestuods@gmail.com>
2022-10-03 16:54:54 -0300

    Adicona o titulo correto para a pagina

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -28,7 +28,7 @@ export default function FavoriteRecipes() {
 
   return (
     <div>
-      <Header title="Done Recipes" />
+      <Header title="Favorite Recipes" />
       <main>
         <div>
           <button
