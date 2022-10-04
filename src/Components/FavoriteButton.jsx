4c94b63b9731712e0487ed1a0896d52dbc02b909
gustavoBarros <gustavo.dutra11@gmail.com>
2022-10-04 18:49:05 -0300

    Ajusta checkPath

--- src/Components/FavoriteButton.jsx
+++ src/Components/FavoriteButton.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import propTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import { FAVORITE_RECIPES } from '../services/variables';
 import saveItem, { getItem } from '../helpers/storage';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
@@ -11,8 +12,11 @@ export default function FavoriteButton({
     idMeal, idDrink, strAlcoholic, strMeal,
     strDrink, strDrinkThumb, strMealThumb,
   },
-  testId, checkPath, idRecipe,
+  testId, idRecipe,
 }) {
+  const history = useHistory();
+  const { location: { pathname } } = history;
+  const checkPath = pathname.includes('meals');
   const getFavRecipes = getItem(FAVORITE_RECIPES) || [];
   const [isFavorite, setIsFavorite] = useState(getFavRecipes
     .some((favRecipe) => favRecipe.id === idRecipe));
@@ -56,6 +60,5 @@ export default function FavoriteButton({
 FavoriteButton.propTypes = {
   recipe: propTypes.objectOf(propTypes.string).isRequired,
   testId: propTypes.string.isRequired,
-  checkPath: propTypes.bool.isRequired,
   idRecipe: propTypes.string.isRequired,
 };
