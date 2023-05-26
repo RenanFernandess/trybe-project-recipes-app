5b1280f0d80dd307d25bb0f08166bd504af0b53d
Renan Fernandes <renzinestuods@gmail.com>
2023-05-26 17:32:41 -0300

    :sparkles: feat: Exporta os principas componentes

---
+++ src/Components/index.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import ShareButton from './ShareButton';
+import FavoriteButton from './FavoriteButton';
+import Footer from './Footer';
+import Recipes from './Recipes';
+import YouTubeEmbed from './YouTubeEmbed';
+
+export default Header;
+export {
+  FavoriteButton,
+  Footer,
+  Recipes,
+  ShareButton,
+  YouTubeEmbed,
+};
