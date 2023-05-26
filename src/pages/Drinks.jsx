a8181e189512929b783f6428f418cf7c26520134
Renan Fernandes <renzinestuods@gmail.com>
2023-05-26 17:35:24 -0300

    :boom: fix: Quando a pagina é montada faz a requisição das receitas e salva no RecipeContext

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,52 +1,20 @@
-import React, { useState, useEffect, useContext } from 'react';
-import propTypes from 'prop-types';
+import React, { useEffect, useContext } from 'react';
 import Header from '../Components/Header';
 import Recipes from '../Components/Recipes';
-import {
-  DRINKS_CATEGORY_ENDPOINT,
-  DRINKS_ENDPOINT,
-  DRINKS_FILTER_BY_CATEGOTY_ENDPOINT,
-} from '../services/variables';
-import { fetchRecipes, fetchCategory } from '../helpers/fetchAPI';
+import { fetchDrinks } from '../helpers/fetchAPI';
 import Footer from '../Components/Footer';
-import appContext from '../context/appContext';
+import RecipeContext from '../context';
 
-export default function Drinks({ history }) {
-  const { URL, setURL, searched, setSearched } = useContext(appContext);
-  const [drinks, setDrinks] = useState([]);
-  const [categorys, setCategorys] = useState([]);
-  const END_POINT = URL || DRINKS_ENDPOINT;
+export default function Drinks() {
+  const { setRecipes } = useContext(RecipeContext);
 
-  useEffect(() => {
-    fetchRecipes(END_POINT, (recipes) => {
-      if (recipes.length === 1 && searched) {
-        setSearched(false);
-        return history.push(`/drinks/${recipes[0].idDrink}`);
-      }
-      setDrinks(recipes);
-    });
-    fetchCategory(DRINKS_CATEGORY_ENDPOINT, setCategorys);
-  }, [END_POINT, URL, history, searched, setSearched]);
-
-  useEffect(() => () => { setURL(''); }, [setURL]);
-
-  const filterByCategory = (category) => {
-    setURL(`${DRINKS_FILTER_BY_CATEGOTY_ENDPOINT}${category}`);
-  };
+  useEffect(() => { fetchDrinks(setRecipes); }, [setRecipes]);
 
   return (
     <div>
       <Header title="Drinks" enableSearchButton />
-      <Recipes
-        recipes={ drinks }
-        categorys={ categorys }
-        filterByCategory={ filterByCategory }
-      />
+      <Recipes />
       <Footer />
     </div>
   );
 }
-
-Drinks.propTypes = {
-  history: propTypes.instanceOf(Object),
-}.isRequired;
