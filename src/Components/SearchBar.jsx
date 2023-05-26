c5b90a0b5af8933ea3b11c93b7c4d67322aeebaa
Renan Fernandes <renzinestuods@gmail.com>
2023-05-26 15:44:46 -0300

    :boom: fix: Faz a requisição das receitas e salva no RecipeContext

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useContext } from 'react';
 import propTypes from 'prop-types';
-import appContext from '../context/appContext';
 import '../css/SearchBar.css';
-import { NAME_LENGTH_ERROR, URLS_BY_PAGE } from '../services/variables';
+import { useHistory } from 'react-router-dom';
+import { NAME_LENGTH_ERROR, RECIPES_ENDPOINT } from '../services/variables';
+import RecipeContext from '../context';
+import fetchAPI from '../helpers/fetchAPI';
 
 export default function SearchBar({ title }) {
-  const { setURL, setSearched } = useContext(appContext);
+  const history = useHistory();
+  const { setRecipe } = useContext(RecipeContext);
   const [{ category, searchTerm }, setState] = useState({
     category: 'name',
     searchTerm: '',
@@ -15,13 +18,20 @@ export default function SearchBar({ title }) {
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const setSearchCategory = () => {
+  const searchRecipes = () => {
     if (searchTerm.length > 1 && category === 'firstLetter') {
       return global.alert(NAME_LENGTH_ERROR);
     }
-    const getEndPoint = `${URLS_BY_PAGE[title][category]}${searchTerm}`;
-    setURL(getEndPoint);
-    setSearched(true);
+    const endpoint = `${RECIPES_ENDPOINT[title][category]}${searchTerm}`;
+    fetchAPI(endpoint, (data) => {
+      if (data.length === 1) {
+        const page = title === 'Meals'
+          ? `/meals/${data[0].idMeal}`
+          : `/drinks/${data[0].idDrink}`;
+        return history.push(page);
+      }
+      setRecipe(data);
+    });
   };
 
   return (
@@ -31,7 +41,7 @@ export default function SearchBar({ title }) {
           type="text"
           data-testid="search-input"
           placeholder="Search"
-          className="search-input"// search-input
+          className="search-input"
           onChange={ handleChange }
           name="searchTerm"
           value={ searchTerm }
@@ -42,7 +52,7 @@ export default function SearchBar({ title }) {
               type="radio"
               data-testid="ingredient-search-radio"
               id="ingredient"
-              className="form-check form-check-inline"// ingredients
+              className="form-check form-check-inline"
               name="category"
               onChange={ handleChange }
               value="ingredient"
@@ -54,7 +64,7 @@ export default function SearchBar({ title }) {
               type="radio"
               data-testid="name-search-radio"
               id="nome"
-              className="name form-check form-check-inline" // name
+              className="name form-check form-check-inline"
               name="category"
               onChange={ handleChange }
               value="nome"
@@ -79,7 +89,7 @@ export default function SearchBar({ title }) {
             id="button"
             name="search-button"
             className="search-button"
-            onClick={ setSearchCategory }
+            onClick={ searchRecipes }
           >
             <div className="search">Busca</div>
           </button>
