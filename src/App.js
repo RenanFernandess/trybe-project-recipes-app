6a60deaf3724e32a5d80f9e63e128e2e1cd7ed8a
Renan Fernandes <renzinestuods@gmail.com>
2023-05-29 16:03:15 -0300

    :sparkles: feat: Configura o RecipeInProgressProvider

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import DoneRecipes from './pages/DoneRecipes';
 import RecipeDetails from './pages/RecipeDetails';
 import RecipeInProgress from './pages/RecipeInProgress';
 import './App.css';
+import { RecipeInProgressProvider } from './context';
 
 function App() {
   return (
@@ -18,10 +19,12 @@ function App() {
       <Route exact path="/meals" component={ Meals } />
       <Route exact path="/drinks" component={ Drinks } />
       <Route exact path="/profile" component={ Profile } />
-      <Route exact path="/meals/:id" component={ RecipeDetails } />
-      <Route exact path="/drinks/:id" component={ RecipeDetails } />
-      <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
-      <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+      <RecipeInProgressProvider>
+        <Route exact path="/meals/:id" component={ RecipeDetails } />
+        <Route exact path="/drinks/:id" component={ RecipeDetails } />
+        <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
+        <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+      </RecipeInProgressProvider>
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
     </Switch>
