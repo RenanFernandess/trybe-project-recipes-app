015609cf4a5aca821185573a9a8202475e43f9b9
Renan Fernandes <renzinestuods@gmail.com>
2023-05-30 16:58:20 -0300

    :bug: fix: Corrige o problema que as rotas "/done-recipes" e "/favorite-recipes" não renderizavam

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function App() {
       <Route exact path="/meals" component={ Meals } />
       <Route exact path="/drinks" component={ Drinks } />
       <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
       <RecipeInProgressProvider>
         <Route exact path="/meals/:id" component={ RecipeDetails } />
         <Route exact path="/drinks/:id" component={ RecipeDetails } />
         <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
         <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
       </RecipeInProgressProvider>
-      <Route exact path="/done-recipes" component={ DoneRecipes } />
-      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
     </Switch>
   );
 }
