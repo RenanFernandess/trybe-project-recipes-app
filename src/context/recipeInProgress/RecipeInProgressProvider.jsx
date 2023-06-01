c9362bd8029fb40e1614b3a82c74b62200499e49
Renan Fernandes <renzinestuods@gmail.com>
2023-06-01 18:49:26 -0300

    :sparkles: feat: Separa o progresso da comida e da bebida; Adiciona somente o progresso da receita atual no contextType

--- src/context/recipeInProgress/RecipeInProgressProvider.jsx
+++ src/context/recipeInProgress/RecipeInProgressProvider.jsx
@@ -8,32 +8,40 @@ import { getIngredients } from '../../helpers';
 export default function RecipeInProgressProvider({ children }) {
   const [recipe, setRecipe] = useState({});
   const ingredients = useMemo(() => getIngredients(recipe), [recipe]);
-  const [progress, setProgress] = useState(getItem(IN_PROGRESS_RECIPES) || {});
-  const recipeId = String(recipe.idMeal || recipe.idDrink);
+  const [progress, setProgress] = useState(
+    getItem(IN_PROGRESS_RECIPES)
+    || { meals: {}, drinks: {} },
+  );
+  const RECIPE_ID = recipe.idMeal || recipe.idDrink;
+  const RECIPE_TYPE = recipe.idMeal ? 'meals' : 'drinks';
 
   useEffect(() => { saveItem(IN_PROGRESS_RECIPES, progress); }, [progress]);
 
   const setRecipeProgress = useCallback((recipeProgress) => {
-    setProgress((state) => ({ ...state, [recipeId]: recipeProgress }));
-  }, [recipeId]);
+    setProgress((state) => {
+      const prog = { ...state };
+      prog[RECIPE_TYPE][RECIPE_ID] = recipeProgress;
+      return prog;
+    });
+  }, [RECIPE_ID, RECIPE_TYPE]);
 
   const clearProgress = useCallback(() => {
-    setProgress(({ [recipeId]: _, ...data }) => data);
-  }, [recipeId]);
-
-  useEffect(() => {
-    if (ingredients.length && !progress[recipeId]) {
-      setRecipeProgress(Array(ingredients.length).fill(false));
-    }
-  }, [recipeId, setRecipeProgress, progress, ingredients]);
+    setProgress((state) => {
+      const { [RECIPE_TYPE]: { [RECIPE_ID]: _, ...data } } = state;
+      const prog = { ...state };
+      prog[RECIPE_TYPE] = data;
+      return prog;
+    });
+  }, [RECIPE_ID, RECIPE_TYPE]);
 
   const contextType = {
     recipe,
     setRecipe,
     ingredients,
-    progress,
+    progress: (progress[RECIPE_TYPE][RECIPE_ID] || Array(ingredients.length).fill(false)),
     setRecipeProgress,
     clearProgress,
+    RECIPE_ID,
   };
 
   return (
