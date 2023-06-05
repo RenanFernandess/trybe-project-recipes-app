89362206d37a386ed2c1312564e5a20133840beb
Renan Fernandes <renzinestuods@gmail.com>
2023-06-05 16:39:52 -0300

    :boom: fix: Remove o Provider

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import Provider from './context/Provider';
 import { RecipeProvider } from './context';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider>
-      <RecipeProvider>
-        <App />
-      </RecipeProvider>
-    </Provider>
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
