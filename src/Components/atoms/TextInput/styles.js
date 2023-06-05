f68578cccee42d4ba2113d141d5184c693f1fe17
Renan Fernandes <renzinestuods@gmail.com>
2023-06-05 19:18:34 -0300

    :lipstick: feat: Adiciona um height de 40px

--- src/Components/atoms/TextInput/styles.js
+++ src/Components/atoms/TextInput/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Input = styled.input`
   border: 1px solid ${({ theme }) => theme.COLORS.PRIMARY};
   border-radius: 5px;
+  height: 40px;
   padding: 5px 10px;
 
   &::placeholder {
