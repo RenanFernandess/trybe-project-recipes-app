8e2c61e91d742e2a8c1673589170232a7b516722
Renan Fernandes <renzinestuods@gmail.com>
2023-06-05 17:55:57 -0300

    :sparkles: feat: Configura o ThemeProvider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { RecipeProvider } from './context';
+import { ThemeProvider } from 'styled-components';
+import light from './theme/light';
 
 ReactDOM.render(
   <BrowserRouter>
-    <RecipeProvider>
-      <App />
-    </RecipeProvider>
+    <ThemeProvider theme={ light }>
+      <RecipeProvider>
+        <App />
+      </RecipeProvider>
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
