d5194eec8683e6c04ee70f3d68d3c0a67edddab7
Renan Fernandes <renzinestuods@gmail.com>
2023-06-06 11:21:12 -0300

    :sparkles: feat: Adiciona icones, logo e imagens

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import logoRecipesApp from './logoRecipesApp.svg';
+import tomatoImage from './tomato.svg';
+import shareIcon from './share.svg';
+import likeIcon from './like.svg';
+import likedIcon from './liked.svg';
+
+export default logoRecipesApp;
+export {
+  tomatoImage,
+  shareIcon,
+  likeIcon,
+  likedIcon,
+};
