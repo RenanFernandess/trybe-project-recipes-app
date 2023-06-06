750eb3ad4f6ab227f54dbaa2ae4354edc922c242
Renan Fernandes <renzinestuods@gmail.com>
2023-06-06 13:51:43 -0300

    :sparkles: feat: Adiciona novos icones

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,11 @@ import tomatoImage from './tomato.svg';
 import shareIcon from './share.svg';
 import likeIcon from './like.svg';
 import likedIcon from './liked.svg';
+import smallLogo from './smallLogo.svg';
+import searchIcon from './searchIcon.svg';
+import profileIcon from './profileIcon.svg';
+import dishIcon from './dishIcon.svg';
+import cupIcon from './cupIcon.svg';
 
 export default logoRecipesApp;
 export {
@@ -10,4 +15,9 @@ export {
   shareIcon,
   likeIcon,
   likedIcon,
+  smallLogo,
+  searchIcon,
+  profileIcon,
+  dishIcon,
+  cupIcon,
 };
