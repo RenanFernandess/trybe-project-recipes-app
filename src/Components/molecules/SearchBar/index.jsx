aaaa3e4dc9a50532956d7e2069257b2c7a710b2c
Renan Fernandes <renzinestuods@gmail.com>
2023-06-06 20:32:27 -0300

    :recycle: refactor: Componentiza os elementos da SearchBar

---
+++ src/Components/molecules/SearchBar/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useContext } from 'react';
+import propTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { NAME_LENGTH_ERROR, RECIPES_ENDPOINT } from '../../../services/variables';
+import RecipeContext from '../../../context';
+import fetchRecipes from '../../../helpers/fetchAPI';
+import TextInput, { Button, RadioInput } from '../../atoms';
+
+export default function SearchBar({ title }) {
+  const history = useHistory();
+  const { setRecipes } = useContext(RecipeContext);
+  const [{ category, searchTerm }, setState] = useState({
+    category: 'name',
+    searchTerm: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const searchRecipes = () => {
+    if (searchTerm.length > 1 && category === 'firstLetter') {
+      return global.alert(NAME_LENGTH_ERROR);
+    }
+    const endpoint = `${RECIPES_ENDPOINT[title][category]}${searchTerm}`;
+    fetchRecipes(endpoint, (data) => {
+      if (data.length === 1) {
+        const page = title === 'Meals'
+          ? `/meals/${data[0].idMeal}`
+          : `/drinks/${data[0].idDrink}`;
+        return history.push(page);
+      }
+      setRecipes(data);
+    });
+  };
+
+  return (
+    <section className="search-bar">
+      <form>
+        <TextInput
+          placeholder="Search"
+          onChange={ handleChange }
+          name="searchTerm"
+          value={ searchTerm }
+        />
+        <div>
+          <RadioInput
+            name="category"
+            onChange={ handleChange }
+            value="ingredient"
+            text="Ingredient"
+          />
+          <RadioInput
+            name="category"
+            onChange={ handleChange }
+            value="nome"
+            text="Name"
+          />
+          <RadioInput
+            name="category"
+            onChange={ handleChange }
+            value="firstLetter"
+            text="First letter"
+          />
+        </div>
+        <Button
+          onClick={ searchRecipes }
+        >
+          SEARCH
+        </Button>
+      </form>
+    </section>
+  );
+}
+
+SearchBar.propTypes = {
+  title: propTypes.string,
+}.isRequired;
--- src/Components/molecules/index.js
+++ src/Components/molecules/index.js
@@ -1,3 +1,7 @@
 import HeaderSection from './HeaderSection';
+import SearchBar from './SearchBar';
 
 export default HeaderSection;
+export {
+  SearchBar,
+};
