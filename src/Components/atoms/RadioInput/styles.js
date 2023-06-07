8383d061c973c03c7c47746e006976e932743ce0
Renan Fernandes <renzinestuods@gmail.com>
2023-06-07 11:54:13 -0300

    :lipstick: feat: Estiliza o input radio

--- src/Components/atoms/RadioInput/styles.js
+++ src/Components/atoms/RadioInput/styles.js
@@ -1,13 +1,30 @@
 import styled from 'styled-components';
 
 const Input = styled.input`
-  /* display: none; */
-  margin-right: 5px;
+  height: 15px;
+  width: 15px;
+
+  &::before {
+    content: "";
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+    border-radius: 50%;
+    height: 100%;
+    display: block;
+    width: 100%;
+  }
+
+  &:checked::before {
+    background-color: ${({ theme }) => theme.COLORS.SECONDARY};
+    border: 3px solid white;
+  }
 `;
 
 export default Input;
 
 export const Label = styled.label`
   cursor: pointer;
-
+  align-items: center;
+  display: flex;
+  gap: 5px;
+  justify-content: center;
 `;
