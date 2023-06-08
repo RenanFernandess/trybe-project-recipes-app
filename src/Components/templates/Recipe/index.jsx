e56b1d7c4f0e68bd47b9dd60107a524ac8e843da
Renan Fernandes <renzinestuods@gmail.com>
2023-06-08 12:41:44 -0300

    :recycle: refactor: Componentiza o footer

--- src/Components/templates/Recipe/index.jsx
+++ src/Components/templates/Recipe/index.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { RecipeInProgressContext } from '../../../context';
-
-import Recommendations from '../../Recommendations';
-import { DONE_RECIPES } from '../../../services/variables';
-import { getItem } from '../../../helpers/storage';
-import { Button } from '../../atoms';
-import { RecipeHeader } from '../../organisms';
+import { RecipeHeader, RecipeRecommendations } from '../../organisms';
 import { drinkIcon, mealIcon } from '../../../assets';
-import { RecipeIngredients, RecipeInstructions, RecipeVideo } from '../../molecules';
+import {
+  RecipeFooter,
+  RecipeIngredients,
+  RecipeInstructions,
+  RecipeVideo,
+} from '../../molecules';
 import Container from './styles';
 
 const PAGE_ICON = {
@@ -17,55 +17,22 @@ const PAGE_ICON = {
 };
 
 export default function Recipe() {
-  const { location: { pathname }, push } = useHistory();
-  const { id } = useParams();
-  const {
-    progress,
-    recipe,
-    ingredients,
-  } = useContext(RecipeInProgressContext);
-  const page = pathname.includes('meals') ? 'meals' : 'drinks';
+  const { recipe, ingredients } = useContext(RecipeInProgressContext);
+  const { location: { pathname } } = useHistory();
 
   const { strYoutube, strInstructions } = recipe;
-
-  const recipeIsDone = (recipeId) => {
-    const doneRecipes = getItem(DONE_RECIPES) || [];
-    const checkTrue = doneRecipes.some((doneRecipe) => doneRecipe.id === recipeId);
-    return checkTrue;
-  };
-
-  const recipeIsInProgress = () => progress.some((bool) => bool);
+  const page = pathname.includes('meals') ? 'meals' : 'drinks';
 
   return (
     <>
       <RecipeHeader icon={ PAGE_ICON[page] } />
       <Container>
-        <RecipeIngredients
-          ingredients={ ingredients }
-        />
-        <RecipeInstructions
-          text={ strInstructions }
-        />
+        <RecipeIngredients ingredients={ ingredients } />
+        <RecipeInstructions text={ strInstructions } />
         { strYoutube && <RecipeVideo url={ strYoutube } />}
-        <Recommendations page={ page } />
+        <RecipeRecommendations page={ page } />
       </Container>
-      <footer>
-        {
-          !recipeIsDone(id)
-            ? (
-              <Button
-                onClick={ () => {
-                  push(`${pathname}/in-progress`);
-                } }
-              >
-                {(recipeIsInProgress()
-                  ? 'Continue Recipe' : 'Start Recipe'
-                )}
-              </Button>
-            )
-            : null
-        }
-      </footer>
+      <RecipeFooter />
     </>
   );
 }
