94b49f6cb8bf60ea9cbe3e66e110d6316eb142c4
Renan Fernandes <renzinestuods@gmail.com>
2023-06-08 12:27:03 -0300

    :lipstick: fix Almenta o tamanho minimo do RecipeCard

--- src/Components/organisms/RecipeCard/styles.js
+++ src/Components/organisms/RecipeCard/styles.js
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const ContainerLink = styled(Link)`
-  flex: 1;
+  flex: 1 1 172;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
   border: 1px solid ${({ theme }) => theme.COLORS.BORDER};
   border-radius: 5px;
-  min-width: 160px;
+  min-width: 172px;
   max-width: 300px;
   text-decoration: none;
 `;
