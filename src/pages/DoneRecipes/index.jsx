522a94e8984af88f13fd0d3eacc3c181da12a3b1
Renan Fernandes <renzinestuods@gmail.com>
2023-06-09 20:11:21 -0300

    :recycle: refactor: Componentiza os elementos da pagina DoneRecipes

--- src/pages/DoneRecipes/index.jsx
+++ src/pages/DoneRecipes/index.jsx
@@ -1,78 +1,20 @@
-import React, { useState } from 'react';
-import Header, { DoneRecipeCard } from '../../Components';
+import React from 'react';
+import Header, { FinishedRecipes, Footer } from '../../Components';
 import { getItem } from '../../helpers/storage';
 import { DONE_RECIPES } from '../../services/variables';
+import { doneRecipesIcon } from '../../assets';
 
 export default function DoneRecipes() {
   const savedRecipes = getItem(DONE_RECIPES) || [];
-  const [recipes, setRecipes] = useState(savedRecipes);
-
-  const filterRecipes = ({ target: { name } }) => {
-    setRecipes(
-      savedRecipes.filter(({ type }) => type === name || name === 'All'),
-    );
-  };
 
   return (
     <div>
-      <Header title="Done Recipes" />
-      <main>
-        <div>
-          <button
-            type="button"
-            data-testid="filter-by-all-btn"
-            name="All"
-            onClick={ filterRecipes }
-          >
-            All
-          </button>
-          <button
-            data-testid="filter-by-meal-btn"
-            type="button"
-            name="meal"
-            onClick={ filterRecipes }
-          >
-            Meals
-          </button>
-          <button
-            data-testid="filter-by-drink-btn"
-            type="button"
-            name="drink"
-            onClick={ filterRecipes }
-          >
-            Drinks
-          </button>
-        </div>
-        <section>
-          {
-            recipes.map(({
-              alcoholicOrNot,
-              category,
-              doneDate,
-              id,
-              image,
-              name,
-              nationality,
-              tags,
-              type,
-            }, index) => (
-              <DoneRecipeCard
-                key={ id }
-                alcoholicOrNot={ alcoholicOrNot }
-                category={ category }
-                date={ doneDate }
-                id={ id }
-                index={ index }
-                image={ image }
-                name={ name }
-                nationality={ nationality }
-                tags={ tags }
-                type={ type }
-              />
-            ))
-          }
-        </section>
-      </main>
+      <Header
+        icon={ doneRecipesIcon }
+        title="Done Recipes"
+      />
+      <FinishedRecipes recipes={ savedRecipes } />
+      <Footer />
     </div>
   );
 }
