53bf52498b9d8c6c46f623b2955a78e798ec29e6
Renan Fernandes <renzinestuods@gmail.com>
2023-06-09 16:54:53 -0300

    :boom: fix: Remove o reset css do bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Meals from './pages/Meals';
-import Drinks from './pages/Drinks';
-import Profile from './pages/Profile';
-import FavoriteRecipes from './pages/FavoriteRecipes';
-import DoneRecipes from './pages/DoneRecipes';
-import RecipeDetails from './pages/RecipeDetails';
-import RecipeInProgress from './pages/RecipeInProgress';
-import './App.css';
+import Login, {
+  DoneRecipes,
+  Drinks,
+  FavoriteRecipes,
+  Meals,
+  Profile,
+  RecipeDetails,
+  RecipeInProgress,
+} from './pages';
 import { RecipeInProgressProvider } from './context';
 
 function App() {
