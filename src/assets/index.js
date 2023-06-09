ec4065a9aa9dcf64c477ce34223cee05ffdf3141
Renan Fernandes <renzinestuods@gmail.com>
2023-06-09 19:50:51 -0300

    :sparkles: feat: Adiciona os icones de categoria da pagaina DoneRecipes e o icone da pagina

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,12 +10,20 @@ import dishIcon from './dishIcon.svg';
 import cupIcon from './cupIcon.svg';
 import drinkIcon from './drink.svg';
 import mealIcon from './meal.svg';
+import doneRecipesIcon from './doneRecipesIcon.svg';
+import allRecipesIcon from './allIcon.svg';
+import foodsIcon from './foodsIcon.svg';
+import drinksIcon from './drinksIcon.svg';
 
 export default logoRecipesApp;
 export {
+  allRecipesIcon,
   cupIcon,
-  drinkIcon,
+  doneRecipesIcon,
   dishIcon,
+  drinkIcon,
+  drinksIcon,
+  foodsIcon,
   likeIcon,
   likedIcon,
   mealIcon,
