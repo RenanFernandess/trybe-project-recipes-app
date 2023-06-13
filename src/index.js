f00bb20e8673d7fe138e2ca889fcfb634485f4ea
Renan Fernandes <renzinestuods@gmail.com>
2023-06-13 16:43:04 -0300

    :sparkles: feat: Configura o FavoriteProvider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { RecipeProvider } from './context';
+import { FavoritesProvider, RecipeProvider } from './context';
 import App from './App';
 import light from './themes/light';
 import './index.css';
@@ -11,7 +11,9 @@ ReactDOM.render(
   <BrowserRouter>
     <ThemeProvider theme={ light }>
       <RecipeProvider>
-        <App />
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
       </RecipeProvider>
     </ThemeProvider>
   </BrowserRouter>,
