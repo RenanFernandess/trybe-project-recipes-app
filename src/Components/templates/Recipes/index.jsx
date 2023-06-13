e8f9f0dc7dc23f41d05b4ba9e7c78edb9602788b
Renan Fernandes <renzinestuods@gmail.com>
2023-06-13 19:16:42 -0300

    :boom: fix: Não utiliza mais o componente SearchByCategory

--- src/Components/templates/Recipes/index.jsx
+++ src/Components/templates/Recipes/index.jsx
@@ -1,41 +1,37 @@
 import React, { useContext } from 'react';
 import { RecipeCard } from '../../organisms';
 import RecipeContext from '../../../context';
-import SearchByCategory from '../../SearchByCategory';
-import Container, { Cards } from './styles';
+import Container from './styles';
 
 export default function Recipes() {
   const { recipes } = useContext(RecipeContext);
 
   return (
     <Container>
-      <SearchByCategory />
-      <Cards>
-        {
-          recipes.map(({
-            strMealThumb,
-            strDrinkThumb,
-            strMeal,
-            strDrink,
-            idMeal,
-            idDrink,
-          }, index) => {
-            const thumb = strMealThumb || strDrinkThumb;
-            const name = strMeal || strDrink;
-            const id = idMeal || idDrink;
+      {
+        recipes.map(({
+          strMealThumb,
+          strDrinkThumb,
+          strMeal,
+          strDrink,
+          idMeal,
+          idDrink,
+        }, index) => {
+          const thumb = strMealThumb || strDrinkThumb;
+          const name = strMeal || strDrink;
+          const id = idMeal || idDrink;
 
-            return (
-              <RecipeCard
-                key={ id }
-                name={ name }
-                thumb={ thumb }
-                index={ index }
-                id={ id }
-              />
-            );
-          })
-        }
-      </Cards>
+          return (
+            <RecipeCard
+              key={ id }
+              name={ name }
+              thumb={ thumb }
+              index={ index }
+              id={ id }
+            />
+          );
+        })
+      }
     </Container>
   );
 }
--- src/Components/templates/Recipes/styles.js
+++ src/Components/templates/Recipes/styles.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 
 const Container = styled.section`
-  padding: 10px 10px 50px 10px;
-`;
-
-export default Container;
-
-export const Cards = styled.div`
+  padding: 10px;
   display: flex;
   flex-wrap: wrap;
   gap: 10px;
+  margin-bottom: 40px;
 `;
+
+export default Container;
