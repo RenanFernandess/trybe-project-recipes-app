249bd7dbf4ff4c1f2e956b14f8bbed495a6046c4
Renan Fernandes <renzinestuods@gmail.com>
2023-06-13 16:42:00 -0300

    :recycle: refactor: Componentiza os elementos da pagina FavoriteRecipes e estiliza

--- src/pages/FavoriteRecipes/index.jsx
+++ src/pages/FavoriteRecipes/index.jsx
@@ -1,88 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import Header, { FavoriteCard } from '../../Components';
-import saveItem, { getItem } from '../../helpers/storage';
-import { FAVORITE_RECIPES } from '../../services/variables';
+import React from 'react';
+import Header, { FavoriteContent, Footer } from '../../Components';
+import { favoriteIcon } from '../../assets';
 
 export default function FavoriteRecipes() {
-  const [recipes, setRecipes] = useState(getItem(FAVORITE_RECIPES) || []);
-  const [filteredRecipes, setFilteredRecipes] = useState([]);
-  const [filterValue, setFilterValue] = useState('All');
-
-  useEffect(() => {
-    setFilteredRecipes(
-      recipes.filter(({ type }) => type === filterValue || filterValue === 'All'),
-    );
-  }, [setFilteredRecipes, recipes, filterValue]);
-
-  const filterRecipes = ({ target: { name } }) => {
-    setFilterValue(name);
-  };
-
-  const removeFavorite = (recipeId) => {
-    const savedRecipes = getItem(FAVORITE_RECIPES);
-    const recipesList = savedRecipes.filter(({ id }) => recipeId !== id);
-    saveItem(FAVORITE_RECIPES, recipesList);
-    setRecipes(recipesList);
-  };
-
   return (
     <div>
-      <Header title="Favorite Recipes" />
-      <main>
-        <div>
-          <button
-            type="button"
-            data-testid="filter-by-all-btn"
-            name="All"
-            onClick={ filterRecipes }
-          >
-            All
-          </button>
-          <button
-            data-testid="filter-by-meal-btn"
-            type="button"
-            name="meal"
-            onClick={ filterRecipes }
-          >
-            Meals
-          </button>
-          <button
-            data-testid="filter-by-drink-btn"
-            type="button"
-            name="drink"
-            onClick={ filterRecipes }
-          >
-            Drinks
-          </button>
-        </div>
-        <section>
-          {
-            filteredRecipes.map(({
-              alcoholicOrNot,
-              category,
-              id,
-              image,
-              name,
-              nationality,
-              type,
-            }, index) => (
-              <FavoriteCard
-                key={ id }
-                alcoholicOrNot={ alcoholicOrNot }
-                category={ category }
-                id={ id }
-                index={ index }
-                image={ image }
-                isFavorite={ recipes.some(({ id: recipeId }) => recipeId === id) }
-                name={ name }
-                nationality={ nationality }
-                removeFavorite={ removeFavorite }
-                type={ type }
-              />
-            ))
-          }
-        </section>
-      </main>
+      <Header
+        icon={ favoriteIcon }
+        title="Favorite Recipes"
+      />
+      <FavoriteContent />
+      <Footer />
     </div>
   );
 }
