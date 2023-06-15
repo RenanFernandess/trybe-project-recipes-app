fb42139651d3c92747a4be6724e9c943089cbd3a
Renan Fernandes <renzinestuods@gmail.com>
2023-06-15 16:04:34 -0300

    :recycle: refactor: Utiliza a função recipeMap para cria o objeto da receita

--- src/Components/molecules/RecipeInProgressFooter/index.jsx
+++ src/Components/molecules/RecipeInProgressFooter/index.jsx
@@ -4,6 +4,7 @@ import { RecipeInProgressContext } from '../../../context';
 import { Button } from '../../atoms';
 import { DONE_RECIPES } from '../../../services/variables';
 import saveItem, { getItem } from '../../../helpers/storage';
+import { recipeMap } from '../../../helpers';
 import Footer from './styles';
 
 export default function RecipeInProgressFooter() {
@@ -12,24 +13,10 @@ export default function RecipeInProgressFooter() {
   const buttonIsDisable = !progress.every((recipeInProg) => recipeInProg);
 
   const finishRecipe = () => {
-    const {
-      strCategory, strAlcoholic, strMeal,
-      strDrink, strDrinkThumb, strMealThumb,
-      strArea, strTags, idMeal, idDrink,
-    } = recipe;
     const doneRecipes = getItem(DONE_RECIPES) || [];
     const date = new Date();
-    const finishedRecipe = {
-      alcoholicOrNot: strAlcoholic,
-      category: strCategory,
-      doneDate: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-      id: idMeal || idDrink,
-      image: strDrinkThumb || strMealThumb,
-      name: strDrink || strMeal,
-      nationality: strArea,
-      tags: [strTags],
-      type: strMeal ? 'meal' : 'drink',
-    };
+    const doneDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    const finishedRecipe = { ...recipeMap(recipe), doneDate };
     saveItem(DONE_RECIPES, [...doneRecipes, finishedRecipe]);
     clearProgress();
     history.push('/done-recipes');
