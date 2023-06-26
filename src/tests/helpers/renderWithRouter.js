60af06a12743ed95d02dabb7b6c08b865895f6b8
Renan Fernandes <renzinestuods@gmail.com>
2023-06-26 17:09:20 -0300

    :boom: fix: Corrige o renderWithRouter

--- src/tests/helpers/renderWithRouter.js
+++ src/tests/helpers/renderWithRouter.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-import Provider from '../../context/Provider';
+import { ThemeProvider } from 'styled-components';
 import { RecipeProvider } from '../../context';
+import light from '../../themes';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return {
     ...render(
-      <Provider>
+      <ThemeProvider theme={ light }>
         <RecipeProvider>
           <Router history={ history }>
             { component }
           </Router>
         </RecipeProvider>
-      </Provider>,
+      </ThemeProvider>,
     ),
     history,
   };
