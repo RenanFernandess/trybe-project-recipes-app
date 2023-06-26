7a0509cc8a2d932b95d0b0ce5957b5da83974ce8
Renan Fernandes <renzinestuods@gmail.com>
2023-06-26 17:14:46 -0300

    :sparkles: feat: Paga a url da pagina de forma dinamica

--- src/Components/molecules/ShareButton/index.jsx
+++ src/Components/molecules/ShareButton/index.jsx
@@ -4,12 +4,14 @@ import { useRouteMatch } from 'react-router-dom';
 import { ButtonIcon } from '../../atoms';
 import { shareIcon } from '../../../assets';
 
+const URL_BASE = document.URL.match(/^\w+:\/\/\w+:\d+/i)[0];
+
 export default function ShareButton({ url }) {
   const { url: defaultUrl } = useRouteMatch();
   const [linkCopied, setLinkCopied] = useState(false);
 
   const copyBoard = () => {
-    navigator.clipboard.writeText(`http://localhost:3000${url || defaultUrl}`);
+    navigator.clipboard.writeText(`${URL_BASE}${url || defaultUrl}`);
     setLinkCopied(true);
   };
 
