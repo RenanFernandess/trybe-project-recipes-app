1e1fe88dac0c7ac744d933efd9077ca06e28355c
Renan Fernandes <renzinestuods@gmail.com>
2023-06-30 17:18:44 -0300

    :test_tube: fix: Utiliza o mock do user em vez de retornar a string do email

--- src/tests/mocks/storageGetItemMock.js
+++ src/tests/mocks/storageGetItemMock.js
@@ -1,10 +1,11 @@
 import DONE_RECIPES_MOCK from './doneRecipesMock';
 import FAVORITE_RECIPES_MOCK from './favoriteRecipesMock';
+import USER_MOCK from './userMock';
 
 function getKeyValue(key) {
   switch (key) {
   case 'user':
-    return 'jose_receitas@gmail.com';
+    return USER_MOCK;
   case 'doneRecipes':
     return DONE_RECIPES_MOCK;
   case 'favoriteRecipes':
