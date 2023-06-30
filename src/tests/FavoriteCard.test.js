5e7d80ba887d18a2b7b1c7878af7d2089a1cfd5c
Renan Fernandes <renzinestuods@gmail.com>
2023-06-30 17:38:10 -0300

    :white_check_mark: test: Testa o FavoriteCard

--- src/tests/FavoriteCard.test.js
+++ src/tests/FavoriteCard.test.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen, within } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
-import fetchTotal from '../../cypress/mocks/fetch';
-import App from '../App';
+import { FavoriteCard } from '../Components/organisms';
+import { FavoritesProvider } from '../context';
 
-describe('Testa o Header', () => {
+describe('Testa o FavoriteCard', () => {
   beforeEach(() => {
-    global.fetch = fetchTotal;
-    jest.spyOn(global, 'fetch');
+    renderWithRouter(
+      <FavoritesProvider>
+        <FavoriteCard
+          alcoholicOrNot="Non alcoholic"
+          category="Other / Unknown"
+          id="12668"
+          image="https://www.thecocktaildb.com/images/media/drink/mvis731484430445.jpg"
+          name="Egg Cream"
+          nationality=""
+          type="drink"
+        />
+      </FavoritesProvider>,
+    );
   });
 
-  afterEach(() => jest.clearAllMocks());
+  it('Verifica se possui a imagem da receita', () => {
+    const IMAGE = screen.getByRole('img', { name: /egg cream image/i });
+    expect(IMAGE).toBeInTheDocument();
+  });
 
-  it('Testa se o Header possui um título, botão de busca e botão de perfil', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/meals/52977');
-    Object.assign(window.navigator, {
-      clipboard: {
-        writeText: jest.fn().mockImplementation(() => Promise.resolve()),
-      },
-    });
-    const buttonShare = screen.getByTestId('share-btn');
-    userEvent.click(buttonShare);
-    expect(window.navigator.clipboard.writeText).toHaveBeenCalled();
-    expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith('http://localhost:3000/meals/52977');
+  it('Verifica se possui o titulo da receita', () => {
+    const RECIPE_TITLE = screen.getByText(/egg cream/i);
+
+    expect(RECIPE_TITLE).toBeInTheDocument();
+  });
+
+  it('Verifica se possui a categoria da receita', () => {
+    const LINK = screen.getByRole('link', { name: /egg cream other \/ unknown non alcoholic/i });
+    const CATEGORY = within(LINK).getByText(/other \/ unknown non alcoholic/i);
+
+    expect(CATEGORY).toBeInTheDocument();
+  });
+
+  it('Verifica se possui um botão de compartilhar', () => {
+    const SHARE_BUTTON = screen.getByRole('button', { name: /share button/i });
+    expect(SHARE_BUTTON).toBeInTheDocument();
+  });
+
+  it('Verifica se possui um botão para favoritar a receita', () => {
+    const FAVORITE_BUTTON = screen.getByRole('img', { name: /favorite button/i });
+    expect(FAVORITE_BUTTON).toBeInTheDocument();
   });
 });
