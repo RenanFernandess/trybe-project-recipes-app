f1d474fb6d861e72c9d8558626836524f009a7d1
Renan Fernandes <renzinestuods@gmail.com>
2023-07-11 17:41:59 -0300

    :white_check_mark: test: Testando a pagina RecipeDetails

--- src/tests/RecipesDetails.test.js
+++ src/tests/RecipesDetails.test.js
@@ -1,175 +1,97 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
-import oneMeal from '../../cypress/mocks/oneMeal';
-import fetchTotal from '../../cypress/mocks/fetch';
-import oneDrink from '../../cypress/mocks/oneDrink';
-import meals from '../../cypress/mocks/meals';
 import App from '../App';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import saveItem from '../helpers/storage';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
+import fetchMock from './mocks/fetchMock';
+import { MEAL_BUREK_MOCK } from './mocks/meals/mealsMock';
 
-const objeto = [{
-  id: '52771',
-  type: 'meal',
-  nationality: 'Italian',
-  category: 'Vegetarian',
-  alcoholicOrNot: '',
-  name: 'Spicy Arrabiata Penne',
-  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-}];
-const mockDrink = [{
-  id: '15997',
-  type: 'drink',
-  nationality: '',
-  alcoholicOrNot: 'Optional alcohol',
-  category: 'Ordinary Drink',
-  name: 'GG',
-  image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-
-}];
-
-const FVT_BTN_TEST_ID = 'favorite-btn';
-const MEAL_PATH = '/meals/52771';
-const DRINK_PATH = '/drinks/15997';
-
-describe('Testa o Reciper', () => {
+describe('Testa a pagina RecipeDetails', () => {
   beforeEach(() => {
     jest.spyOn(global, 'fetch');
-    global.fetch.mockResolvedValue({
-      json: jest.fn()
-        .mockResolvedValue(fetchTotal)
-        .mockResolvedValueOnce(oneMeal)
-        .mockResolvedValueOnce(meals)
-        .mockResolvedValueOnce(oneDrink),
-    });
-    global.localStorage = jest.fn(saveItem);
-  });
-  afterEach(() => jest.clearAllMocks());
-
-  it('Testa o local storage', async () => {
-    const localStorageMock = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      clear: jest.fn(),
-    };
-    global.localStorage = localStorageMock;
-    localStorage.setItem('FavoriteRecipes', JSON.stringify(objeto));
+    global.fetch = fetchMock;
 
     const { history } = renderWithRouter(<App />);
-    history.push(MEAL_PATH);
-
-    console.log(history.location.pathname);
-
-    const favoriteBtn = await screen.findByTestId(FVT_BTN_TEST_ID);
-    expect(favoriteBtn).toBeInTheDocument();
-    const teste = JSON.parse(localStorage.getItem('FavoriteRecipes'));
-    userEvent.click(favoriteBtn);
-    expect(teste).toHaveLength(1);
+    history.push('/meals/53060');
   });
-  it('Testa os botões ', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/meals');
-    const drinksBtn = screen.getByTestId('drinks-bottom-btn');
-    const mealsBtn = screen.getByTestId('meals-bottom-btn');
-    expect(drinksBtn).toBeInTheDocument();
-    expect(mealsBtn).toBeInTheDocument();
-    userEvent.click(drinksBtn);
-    userEvent.click(mealsBtn);
-    history.push('/done-recipes');
-    const filterByAll = screen.getByTestId('filter-by-all-btn');
-    const filterByMeal = screen.getByTestId('filter-by-meal-btn');
-    const filterByDrink = screen.getByTestId('filter-by-drink-btn');
-    expect(filterByAll).toBeInTheDocument();
-    expect(filterByMeal).toBeInTheDocument();
-    expect(filterByDrink).toBeInTheDocument();
-    userEvent.click(filterByAll);
-    userEvent.click(filterByMeal);
-    userEvent.click(filterByDrink);
-  });
-  it('Se aparece as receitas em progresso', async () => {
-    const localStorageMock = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      clear: jest.fn(),
-    };
-    global.localStorage = localStorageMock;
-    localStorage.setItem('inProgressRecipes', JSON.stringify(objeto));
 
-    const { history } = renderWithRouter(<App />);
-    history.push(MEAL_PATH);
-    await waitFor(() => {
-      const favoriteBtn = screen.getByTestId(FVT_BTN_TEST_ID);
-      const startRecipe = screen.getByTestId('start-recipe-btn');
-      userEvent.click(startRecipe);
-      expect(favoriteBtn).toBeInTheDocument();
-      const transforme = JSON.parse(localStorage.getItem('inProgressRecipes'));
-      userEvent.click(favoriteBtn);
-      expect(transforme).toHaveLength(1);
-    });
+  afterEach(() => {
+    jest.clearAllMocks();
+    localStorage.clear();
   });
-  it('Testa se o link é copiado', async () => {
-    const { history } = renderWithRouter(<App />);
-    history.push(MEAL_PATH);
-    Object.assign(navigator, {
-      clipboard: {
-        writeText: () => { 'Link copied!'; },
-      },
-    });
-    jest.spyOn(navigator.clipboard, 'writeText');
-    const shareBtn = screen.getByTestId('share-btn');
-    userEvent.click(shareBtn);
-    const message = screen.getByText(/link copied!/i);
-    expect(message).toBeInTheDocument();
-    const URL = 'http://localhost:3000/meals/52771';
-    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(URL);
-  });
-  it('Salvar nos favoritos', async () => {
-    const localStorageMock = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      clear: jest.fn(),
-    };
-    global.localStorage = localStorageMock;
-    localStorage.setItem('favoriteRecipes', JSON.stringify(mockDrink));
 
-    const { history } = renderWithRouter(<App />);
-    history.push(DRINK_PATH);
-    const favorite = await screen.findByTestId(FVT_BTN_TEST_ID);
-    expect(favorite).toHaveAttribute('src', blackHeartIcon);
-    userEvent.click(favorite);
-    expect(favorite).toHaveAttribute('src', whiteHeartIcon);
-    const transforme = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    expect(transforme).toHaveLength(0);
+  it('Verifica se a pagina possui um header com o titulo da receita', async () => {
+    const title = await screen.findByRole('heading', { name: /burek/i });
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent('Burek');
   });
-  it('Se direciona para a pagina in-progress', async () => {
-    const localStorageMock = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      clear: jest.fn(),
-    };
-    global.localStorage = localStorageMock;
-    localStorage.setItem('favoriteRecipes', JSON.stringify(mockDrink));
 
-    const { history } = renderWithRouter(<App />);
-    history.push(DRINK_PATH);
-    const startRecipe = screen.getByTestId('start-recipe-btn');
-    expect(startRecipe).toBeInTheDocument();
-    userEvent.click(startRecipe);
-    expect(history.location.pathname).toBe('/drinks/15997/in-progress');
+  it('Verifica se possui um botão para compartilhar a receita e outro para favoritar', async () => {
+    const shareButton = await screen.findByRole('button', { name: /share button/i });
+    const favoriteButton = screen.getByRole('button', { name: /favorite button/i });
+    expect(shareButton).toBeInTheDocument();
+    expect(favoriteButton).toBeInTheDocument();
   });
-  it('Se há o direcionamento para o id da receita ', () => {
-    const localStorageMock = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-      clear: jest.fn(),
-    };
-    global.localStorage = localStorageMock;
-    localStorage.setItem('favoriteRecipes', JSON.stringify(mockDrink));
 
-    const { history } = renderWithRouter(<App />);
-    history.push(DRINK_PATH);
+  it('Verifica se possui a categoria da receita', async () => {
+    const category = await screen.findByText(/side/i);
+    expect(category).toBeInTheDocument();
+    expect(category).toHaveTextContent('Side');
+  });
+
+  it('Verifica se possui uma lista de ingredientes e o titulo ingredients', async () => {
+    const ingredientsTitle = await screen.findByRole('heading', { name: /ingredients/i });
+    expect(ingredientsTitle).toBeInTheDocument();
+    expect(ingredientsTitle).toHaveTextContent('Ingredients');
+
+    const ingredients = screen.getAllByRole('listitem');
+    expect(ingredients).toHaveLength(6);
+    expect(ingredients[0]).toHaveTextContent('Filo Pastry: 1 Packet');
+    expect(ingredients[1]).toHaveTextContent('Minced Beef: 150g');
+    expect(ingredients[2]).toHaveTextContent('Onion: 150g');
+    expect(ingredients[3]).toHaveTextContent('Oil: 40g');
+    expect(ingredients[4]).toHaveTextContent('Salt: Dash');
+    expect(ingredients[5]).toHaveTextContent('Pepper: Dash');
+  });
+
+  it('Verifica se possui instruções de preparo', async () => {
+    const instructionsTitle = await screen.findByRole('heading', { name: /instructions/i });
+    expect(instructionsTitle).toBeInTheDocument();
+    expect(instructionsTitle).toHaveTextContent('Instructions');
+
+    const instructions = screen.getByText(/^Fry the finely chopped onions and minced meat in oil/i);
+    expect(instructions).toBeInTheDocument();
+    expect(instructions).toHaveTextContent(MEAL_BUREK_MOCK.meals[0].strInstructions);
+  });
+
+  it('Verifica se possui um video com a receita', async () => {
+    const videoTitle = await screen.findByRole('heading', { name: /video/i });
+    expect(videoTitle).toBeInTheDocument();
+    expect(videoTitle).toHaveTextContent('Video');
+
+    const video = screen.getByTitle(/embedded youtube/i);
+    expect(video).toBeInTheDocument();
+    expect(video).toHaveAttribute('src', 'https://www.youtube.com/embed/YsJXZwE5pdY');
+  });
+
+  it('Verifica se possui uma lista de receitas de bebidas recomendadas', async () => {
+    const recommendedTitle = await screen.findByRole('heading', { name: /recommended/i });
+    expect(recommendedTitle).toBeInTheDocument();
+    expect(recommendedTitle).toHaveTextContent('Recommended');
+
+    const recommended = screen.getAllByRole('link');
+    expect(recommended).toHaveLength(6);
+    expect(recommended[0]).toHaveTextContent(/GG/i);
+    expect(recommended[1]).toHaveTextContent(/A1/i);
+    expect(recommended[2]).toHaveTextContent(/Ace/i);
+    expect(recommended[3]).toHaveTextContent(/747/i);
+    expect(recommended[4]).toHaveTextContent(/Kir/i);
+    expect(recommended[5]).toHaveTextContent(/ABC/i);
+  });
+
+  it('Verifica se possui um botão para iniciar o preparo da receita', async () => {
+    const startRecipeButton = await screen.findByRole('button', { name: /start recipe/i });
+    expect(startRecipeButton).toBeInTheDocument();
+    expect(startRecipeButton).toHaveTextContent('Start Recipe');
   });
 });
